268cb707bd0d6b875ec6049eb84be0bb2b8741be
aggustinn <aalnahas.lego@gmail.com>
2024-02-21 07:51:23 +0300

    Initial commit

---
+++ src/main/java/net/phoillows/rot/RelicsOfTime.java
@@ -0,0 +1,32 @@
+package net.phoillows.rot;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(RelicsOfTime.MODID)
+public class RelicsOfTime {
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "rot";
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public RelicsOfTime() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        // Register the commonSetup method for modloading
+        modEventBus.addListener(this::commonSetup);
+
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+    }
+}
